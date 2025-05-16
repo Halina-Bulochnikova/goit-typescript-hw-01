d31697ffb4422254efefb0e10ecdab16fe9026d4
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-05-16 19:54:24 +0200

    add ts files

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+let someValue: string | number;
+
+someValue = 'summer';
+
+console.log(someValue);
+
+someValue = 2025;
+
+console.log(someValue);
+
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 import './basic/1';
+import './basic/2';
+import './basic/3';
+import "./basic/4";
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
